87d0c30415019f8a700b0e39545e8b21aeba1afa
Chikondi Nkhoma <61491585+NkhomaChikondi@users.noreply.github.com>
2023-09-04 13:53:13 +0200

    Added data manager and verseProcessor interfaces and their implementations

--- Interfaces/IVerseProcessor.cs
+++ Interfaces/IVerseProcessor.cs
@@ -4,13 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Apple_Banana
+namespace Apple_Banana.Interfaces
 {
-    internal class Program
+    internal interface IVerseProcessor
     {
-        static void Main(string[] args)
-        {
-
-        }
     }
 }
